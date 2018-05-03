7e945ff7f61ed407aecebd56288500411230e043
jovanperunovic <jovanperunovic1@gmail.com>
2018-05-03 11:41:38 +0200

    Added onClick listener for Start button

--- app/src/main/java/com/example/korisnik/project1/MainActivity.java
+++ app/src/main/java/com/example/korisnik/project1/MainActivity.java
@@ -2,12 +2,25 @@ package com.example.korisnik.project1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+
+    public void onDisplay(View v){
+
+
+        Toast.makeText(MainActivity.this,"First Toast Ever", Toast.LENGTH_LONG).show();
     }
 }
